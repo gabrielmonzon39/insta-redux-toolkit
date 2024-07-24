72d7c0c9434b0f6c39a63b0dcfb0a6cd0b5d64dd
Gabriel Monzon <gabriel.monzon39@gmail.com>
2024-07-24 03:12:51 -0600

    add: redux usage on gallery components

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,35 +1,39 @@
-import React from 'react'
-import getPhotoUrl from 'get-photo-url'
-import { posts } from '../data'
+import React from "react";
+import getPhotoUrl from "get-photo-url";
+import { useDispatch, useSelector } from "react-redux";
+import { galleryActions } from "../data/slice";
 
 const Gallery = () => {
-  const addPhoto = async () => {
-    const imageUrl = await getPhotoUrl('#addPhotoInput')
-    const post = {
-      id: '',
-      imageUrl,
-      text: ''
-    }
-  }
+  const galleryState = useSelector((state) => state.gallery);
+  const dispatch = useDispatch();
 
-  const galleryItems = posts.map(post => (
+  const addPhoto = async () => {
+    const imageUrl = await getPhotoUrl("#addPhotoInput");
+    const post = {
+      id: "",
+      imageUrl,
+      text: "",
+    };
+
+    dispatch(galleryActions.addPhoto(post));
+  };
+
+  const galleryItems = galleryState.posts.map((post) => (
     <div className="item" key={post.id} tabIndex={8}>
       <img src={post.imageUrl} alt="" />
     </div>
-  ))
+  ));
 
   return (
     <>
       <input type="file" name="photo" id="addPhotoInput" />
       <label htmlFor="addPhotoInput">
-        <i className="add-photo fas fa-plus-square" onClick={addPhoto}/>
+        <i className="add-photo fas fa-plus-square" onClick={addPhoto} />
       </label>
       <div className="gallery-nav" />
-      <section className="gallery">
-        {galleryItems}
-      </section>
+      <section className="gallery">{galleryItems}</section>
     </>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
